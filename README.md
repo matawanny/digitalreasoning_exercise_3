e08baa20a94b542eefe3d229e654117a2c63a2a3
Phillip Xiao <phillip.f.xiao@gmail.com>
2015-04-06 12:48:04 -0400

    improve code with callable and future and get aggregated xml output

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
 #Excercise 3
 run mvn install
-run App and you will get an xml file output as temp/nlp_data/d01.xml, d02.xml...d10.xml
-
+run App and you will get individual xml file output as temp/nlp_data/d01.xml, d02.xml...d10.xml
+run AppFuture and you will get aggregated xml output as nlp_data.xml  
--- src/main/java/com/digitalreasoning/exercise_3/AppFuture.java
+++ src/main/java/com/digitalreasoning/exercise_3/AppFuture.java
@@ -1,32 +1,40 @@
 package com.digitalreasoning.exercise_3;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
 import java.util.regex.Pattern;
 
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
-import com.digitalreasoning.exercise_3.FileScanner;
+import com.digitalreasoning.nlf.ObjectFactory;
+import com.digitalreasoning.nlf.Sentenceset;
+import com.digitalreasoning.nlf.TSentence;
 
 /**
  * @author Fei Xiao
  *
  */
-class Processor implements Runnable {
+class CallableProcessor implements Callable<Integer> {
     
     private int id;
     private List<Pattern> dic;
     private String fileName;
     private String xmlFileName;
     
-    public Processor(int id, List<Pattern> dic) {
+    public CallableProcessor(int id, List<Pattern> dic) {
         this.id = id;
         this.dic= dic;
 
@@ -40,7 +48,7 @@ class Processor implements Runnable {
         
     }
     
-    public void run() {
+    public Integer call() throws Exception {
         System.out.println("Starting process" + fileName);
         
 		FileScanner fs = new FileScanner(fileName, xmlFileName);
@@ -60,13 +68,15 @@ class Processor implements Runnable {
 		};
         
         System.out.println("Process " + fileName + " Completed: " + id);
+        
+        return id;
     }
 }
 
 
-public class App {
+public class AppFuture {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JAXBException {
     	
     	
     	String sourceFile = "nlp_data.zip";
@@ -92,23 +102,52 @@ public class App {
         	dic.add(p);
 
         }
-	    scannerDictionary.close();    	
-    	
-        
+	    scannerDictionary.close();
+	    
+      
        ExecutorService executor = Executors.newFixedThreadPool(4);
+       
+       List<Future> ls = new ArrayList<Future>();
         
         for(int i=1; i<=10; i++) {
-            executor.submit(new CallableProcessor(i, dic));
+        	Future<Integer> future = executor.submit(new CallableProcessor(i, dic));
+        	ls.add(future);
         }
         
         executor.shutdown();
         
         System.out.println("All tasks submitted.");
         
+		JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+	    Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+	    String xmlFileName = null;
+		Sentenceset merged = new Sentenceset();
         try {
-            executor.awaitTermination(1, TimeUnit.DAYS);
+        	for(Future future: ls){
+        		Integer result = (Integer)future.get(); 
+                System.out.println("Result is: " + result);
+                
+                if (result<10)
+                	xmlFileName =  "temp/nlp_data/d0" + result + ".xml";
+                else
+                	xmlFileName =  "temp/nlp_data/d" + result + ".xml";
+   		        
+                Sentenceset sentenceset = (Sentenceset) unmarshaller.unmarshal(new File(xmlFileName));
+   		    
+	   		    List<TSentence> sentences = sentenceset.getSentence();
+	   		    if (sentences!=null && sentences.size()>0){
+	   	   		    merged.getSentence().addAll(sentences);
+	   		    }
+        	}
+        	
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e1) {
+            e1.printStackTrace();
         }
+        
+        Marshaller marshaller = jaxbContext.createMarshaller();
+	    marshaller.marshal(merged, new File("nlp_data.xml"));
         
         System.out.println("All tasks completed.");
     }
